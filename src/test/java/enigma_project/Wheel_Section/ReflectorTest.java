548b48d32dbf413a6d3ab61c8fd5891a53f2a226
flippi321 <90700334+flippi321@users.noreply.github.com>
2022-11-07 20:47:06 +0100

    Finished Wheel_type_Test

--- src/test/java/enigma_project/Wheel_Section/ReflectorTest.java
+++ src/test/java/enigma_project/Wheel_Section/ReflectorTest.java
@@ -1,4 +1,15 @@
 package enigma_project.Wheel_Section;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class ReflectorTest {
+
 }
--- src/test/java/enigma_project/Wheel_Section/Wheel_type_Test.java
+++ src/test/java/enigma_project/Wheel_Section/Wheel_type_Test.java
@@ -1,4 +1,47 @@
 package enigma_project.Wheel_Section;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class Wheel_type_Test {
+    @Nested
+    @DisplayName("GetName commands")
+    class GetNameCommands {
+        @Test
+        @DisplayName("Check Drum with Setting A")
+        public void checkWheelTypeA() {
+            try {
+                Wheel_type type = Wheel_type.A;
+                assertEquals("A", type.name());
+            } catch (IllegalArgumentException e) {
+                fail("checkWheelTypeA");
+            }
+        }
+
+        @Test
+        @DisplayName("Check Drum with Setting B")
+        public void checkWheelTypeB() {
+            try {
+                Wheel_type type = Wheel_type.B;
+                assertEquals("B", type.name());
+            } catch (IllegalArgumentException e) {
+                fail("checkWheelTypeB");
+            }
+        }
+
+        @Test
+        @DisplayName("Check Drum with Setting C")
+        public void checkWheelTypeC() {
+            try {
+                Wheel_type type = Wheel_type.C;
+                assertEquals("C", type.name());
+            } catch (IllegalArgumentException e) {
+                fail("checkWheelTypeC");
+            }
+        }
+    }
 }
